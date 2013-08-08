c392799ad37cafd4afa9633db0e49883c0851f73
Marvin Nikijuluw <marvin8214@yahoo.com>
2013-08-08 12:06:46 -0500

    Update flipit.rb

--- flipit.rb
+++ flipit.rb
@@ -1,5 +1,5 @@
 # Flip the coin 
-# The rules is very simple. The player makes a bet which side of the coin is going to be on the up side. 
+# The rule is very simple. The player makes a bet which side of the coin is going to be on the up side. 
 # If the player makes the right bet he/she wins. If not, he/she loses. 
 
 puts "What's your name?"
