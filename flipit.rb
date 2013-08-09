6b29e278efc2d1d92d851e5d9099174037d201ca
Marvin Nikijuluw <marvin8214@yahoo.com>
2013-08-08 22:27:44 -0500

    Update flipit.rb

--- flipit.rb
+++ flipit.rb
@@ -1,34 +1,34 @@
 # Flip the coin 
 # The rules is very simple. The player makes a bet which side of the coin is going to be on the up side. 
-# If the player makes the right bet he/she wins. If not, player loses. 
+# If the player makes the right bet hplaye wins. If not, player loses. 
 
 puts "What's your name?"
 name = gets.chomp.upcase
 puts "Welcome to the game #{name} :)"
 
-head = "1"
-tail = "2"
-
-begin 
+while true
 	puts "Now, go ahead  make your bet. 1)head or 2)tail ?"
-	answer = gets.chomp 
-	end while !["1","2"].include?(answer)
+	answer = gets.chomp.to_i 
+	["1","2"].include?(answer)
 
-if answer == "1"
-	rand(1..2) 
-	if rand(1..2) == "1" 
-		puts "Congratulations #{name}, you win the game"
-	else 
-		puts "Sorry it's the TAIL, better luck next time :)"
+	if answer == "2".to_i 
+		if rand(2) != "1".to_i
+			puts "Congratulations #{name}, you win the game"
+		else 
+			puts "Sorry it's the TAIL, better luck next time :)"
+		end 
+		break
 	end 
-elsif answer == "2"
-	rand(1..2)
-	if rand(1..2) == "2"
-		puts "Congratulations #{name}, you win the game"
-	else 
-		puts "Sorry #{name}, not today! It's the HEAD!!!"
+	if answer == "1".to_i
+		if rand(2) != "1".to_i
+			puts "Congratulations #{name}, you win the game"
+		else 
+			puts "Sorry #{name}, not today! It's the HEAD!!!"
+		end 
+		break
 	end 
 end 
+
 
 
 
